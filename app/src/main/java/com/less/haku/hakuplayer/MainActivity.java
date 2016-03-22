fc863d47811adfc5b29958bb6a9c67f18d667dae
HaKu <335279429@qq.com>
2016-03-22 17:09:32 +0800

    add danmaku with image

--- app/src/main/java/com/less/haku/hakuplayer/MainActivity.java
+++ app/src/main/java/com/less/haku/hakuplayer/MainActivity.java
@@ -1,9 +1,15 @@
 package com.less.haku.hakuplayer;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -14,6 +20,7 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
+import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,8 +32,11 @@ import master.flame.danmaku.danmaku.loader.android.DanmakuLoaderFactory;
 import master.flame.danmaku.danmaku.model.BaseDanmaku;
 import master.flame.danmaku.danmaku.model.DanmakuTimer;
 import master.flame.danmaku.danmaku.model.IDanmakus;
+import master.flame.danmaku.danmaku.model.IDisplayer;
+import master.flame.danmaku.danmaku.model.android.BaseCacheStuffer;
 import master.flame.danmaku.danmaku.model.android.DanmakuContext;
 import master.flame.danmaku.danmaku.model.android.Danmakus;
+import master.flame.danmaku.danmaku.model.android.SpannedCacheStuffer;
 import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
 import master.flame.danmaku.danmaku.parser.IDataSource;
 import master.flame.danmaku.danmaku.parser.android.BiliDanmukuParser;
@@ -59,6 +69,56 @@ public class MainActivity extends AppCompatActivity {
     private DanmakuContext context;
     private BaseDanmakuParser mParser;
 
+    private BaseCacheStuffer.Proxy mCacheStufferAdapter = new BaseCacheStuffer.Proxy() {
+
+        private Drawable mDrawable;
+
+        @Override
+        public void prepareDrawing(final BaseDanmaku danmaku, boolean fromWorkerThread) {
+            if (danmaku.text instanceof Spanned) { // 根据你的条件检查是否需要需要更新弹幕
+                // FIXME 这里只是简单启个线程来加载远程url图片，请使用你自己的异步线程池，最好加上你的缓存池
+                new Thread() {
+
+                    @Override
+                    public void run() {
+//                        String url = "http://www.bilibili.com/favicon.ico";
+//                        InputStream inputStream = null;
+//                        Drawable drawable = mDrawable;
+//                        if(drawable == null) {
+//                            try {
+//                                URLConnection urlConnection = new URL(url).openConnection();
+//                                inputStream = urlConnection.getInputStream();
+//                                drawable = BitmapDrawable.createFromStream(inputStream, "bitmap");
+//                                mDrawable = drawable;
+//                            } catch (MalformedURLException e) {
+//                                e.printStackTrace();
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            } finally {
+//                                IOUtils.closeQuietly(inputStream);
+//                            }
+//                        }
+                        Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher);
+                        if (drawable != null) {
+                            drawable.setBounds(0, 0, 100, 100);
+                            SpannableStringBuilder spannable = createSpannable(drawable);
+                            danmaku.text = spannable;
+                            if(danmakuView != null) {
+                                danmakuView.invalidateDanmaku(danmaku, false);
+                            }
+                            return;
+                        }
+                    }
+                }.start();
+            }
+        }
+
+        @Override
+        public void releaseResource(BaseDanmaku danmaku) {
+            // TODO 重要:清理含有ImageSpan的text中的一些占用内存的资源 例如drawable
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,6 +129,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        // 设置最大显示行数
+        HashMap<Integer, Integer> maxLinesPair = new HashMap<Integer, Integer>();
+        maxLinesPair.put(BaseDanmaku.TYPE_SCROLL_RL, 5); // 滚动弹幕最大显示3行
+
+        // 设置是否禁止重叠
+        HashMap<Integer, Boolean> overlappingEnablePair = new HashMap<Integer, Boolean>();
+        overlappingEnablePair.put(BaseDanmaku.TYPE_SCROLL_RL, false);
+        overlappingEnablePair.put(BaseDanmaku.TYPE_FIX_TOP, false);
+
+
         context = DanmakuContext.create();
 
         client = new OkHttpClient();
@@ -76,6 +146,14 @@ public class MainActivity extends AppCompatActivity {
         ijkMediaPlayer = new IjkMediaPlayer();
         ijkMediaPlayer.setKeepInBackground(false);
 
+        context.setDanmakuStyle(IDisplayer.DANMAKU_STYLE_STROKEN, 3)
+                .setDuplicateMergingEnabled(false)
+                .setScrollSpeedFactor(1.2f)
+                .setScaleTextSize(1.2f)
+                .setCacheStuffer(new SpannedCacheStuffer(), mCacheStufferAdapter) // 图文混排使用SpannedCacheStuffer
+//        .setCacheStuffer(new BackgroundCacheStuffer())  // 绘制背景使用BackgroundCacheStuffer
+                .setMaximumLines(maxLinesPair)
+                .preventOverlapping(overlappingEnablePair);
 
         if (danmakuView != null) {
             mParser = createParser(null);
@@ -255,7 +333,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 增加弹幕
      */
-    @OnClick(R.id.danmaku_send)
+
     void addDanmaku() {
         BaseDanmaku danmaku = context.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL, context);
 //        BaseDanmaku danmaku = context.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
@@ -264,7 +342,7 @@ public class MainActivity extends AppCompatActivity {
         }
         // for(int i=0;i<100;i++){
         // }
-        danmaku.text = "这是一条弹幕" + System.nanoTime();
+        danmaku.text = "这是一条弹幕 ++++ \n" + System.nanoTime();
         danmaku.padding = 5;
         danmaku.priority = 0;  // 可能会被各种过滤器过滤并隐藏显示
         danmaku.isLive = false;
@@ -277,6 +355,45 @@ public class MainActivity extends AppCompatActivity {
         danmaku.borderColor = Color.GREEN;
         danmakuView.addDanmaku(danmaku);
 
+    }
+
+    @OnClick(R.id.danmaku_send)
+    void addMulti() {
+        for (int i = 0; i < 5; i++)  {
+            addDanmaKuShowTextAndImage();
+        }
+    }
+
+    void addDanmaKuShowTextAndImage() {
+        BaseDanmaku danmaku = context.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL, context);
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher);
+        drawable.setBounds(0, 0, 100, 100);
+        SpannableStringBuilder spannable = createSpannable(drawable);
+        danmaku.text = spannable;
+        danmaku.padding = 5;
+        danmaku.priority = 1;  // 一定会显示, 一般用于本机发送的弹幕
+        danmaku.isLive = false;
+        danmaku.time = danmakuView.getCurrentTime() + 1200;
+        danmaku.textSize = 25f * (mParser.getDisplayer().getDensity() - 0.6f);
+        danmaku.textColor = Color.RED;
+        danmaku.textShadowColor = 0; // 重要：如果有图文混排，最好不要设置描边(设textShadowColor=0)，否则会进行两次复杂的绘制导致运行效率降低
+        danmaku.underlineColor = Color.GREEN;
+        danmakuView.addDanmaku(danmaku);
+
+//        danmuText.setText(spannable);
+    }
+
+    private SpannableStringBuilder createSpannable(Drawable drawable) {
+        String text = "bitmap";
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(text);
+        ImageSpan span = new ImageSpan(drawable);//ImageSpan.ALIGN_BOTTOM);
+        spannableStringBuilder.setSpan(span, 0, 1, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        spannableStringBuilder.append("图文混排111" + System.nanoTime());
+
+//        spannableStringBuilder.setSpan(span, 2, 3, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+//        spannableStringBuilder.append(spannableStringBuilder);
+        spannableStringBuilder.setSpan(new BackgroundColorSpan(Color.parseColor("#8A2233B1")), 0, spannableStringBuilder.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        return spannableStringBuilder;
     }
 
     private BaseDanmakuParser createParser(InputStream stream) {
@@ -304,4 +421,5 @@ public class MainActivity extends AppCompatActivity {
         return parser;
 
     }
+
 }
